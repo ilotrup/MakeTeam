05ed6c4d6c85879c0ca0cab51ac2d82a84a12d8d
NICOLAS GOLDFARB <44362188@comunidadort.edu.ar>
2019-11-07 09:05:28 -0300

    Home Controller y Site.css

--- maketeam/Controllers/HomeController.cs
+++ maketeam/Controllers/HomeController.cs
@@ -10,9 +10,12 @@ namespace maketeam.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Mensaje = "Hola todo bien";
+            ViewBag.Descripcion1 = "Guarda y accede a tus equipos ya armados . ¡Ojo! Hay una cantida maxima de equipos por persona, ¡no te lo pierdas!";
+            ViewBag.Descripcion2 = "¿Queres tener un equipo soñado con tus amigos?";
+            ViewBag.Descripcion22 = "Con este sitio todo es posible. Ingresando tus datos ya vas a poder cumplir tu sueño.";
             return View();
         }
+
 
         public ActionResult About()
         {
