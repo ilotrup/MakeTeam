77ea6d7f16746ad1fb2c64e1d269803421c6389e
damigluk <damigluk@gmail.com>
2019-11-12 11:08:14 -0300

    AAAA

--- maketeam/Models/BD.cs
+++ maketeam/Models/BD.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+
 namespace maketeam.Models
 {
     public static class BD
     {
         private static SqlConnection Conectar()
         {
-            string constring = "Server=.; DataBase = Juego; Trusted_Connection = true;";
+            string constring = "Server=.; DataBase = MakeTeam; Trusted_Connection = true;";
             SqlConnection a = new SqlConnection(constring);
             a.Open();
             return a;
@@ -18,6 +19,26 @@ namespace maketeam.Models
         private static void Desconectar(SqlConnection con)
         {
             con.Close();
+        }
+
+        public static Boolean Loguearse(Usuario User)
+        {
+            bool Logueado;
+            SqlConnection Con = Conectar();
+            SqlCommand consulta = Con.CreateCommand();
+            consulta.CommandType = System.Data.CommandType.Text;
+            consulta.CommandText = "Select * from Usuarios where NombreDeUsuario LIKE '" + User.NombreDeUsuario + "' and Contraseña LIKE '" + User.Contraseña + "'";
+            SqlDataReader lector = consulta.ExecuteReader();
+            Usuario Us = new Usuario();
+            if (lector.Read())
+            {
+                Logueado = true;
+            }
+            else
+            {
+                Logueado = false;
+            }
+            return Logueado;
         }
     }
 }
