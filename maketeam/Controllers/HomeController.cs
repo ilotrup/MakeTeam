88d5907b27fe0c1ccd36f2bb1dd3b4ce97e83131
NICOLAS GOLDFARB <44362188@comunidadort.edu.ar>
2019-11-12 11:07:19 -0300

    hola

--- maketeam/Controllers/HomeController.cs
+++ maketeam/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using maketeam.Models;
 
 namespace maketeam.Controllers
 {
@@ -42,6 +43,11 @@ namespace maketeam.Controllers
         }
 
         public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
         {
             return View();
         }
--- maketeam/Models/BD.cs
+++ maketeam/Models/BD.cs
@@ -9,7 +9,7 @@ namespace maketeam.Models
     {
         private static SqlConnection Conectar()
         {
-            string constring = "Server=.; DataBase = Juego; Trusted_Connection = true;";
+            string constring = "Server=.; DataBase = MakeTeam; Trusted_Connection = true;";
             SqlConnection a = new SqlConnection(constring);
             a.Open();
             return a;
@@ -18,6 +18,26 @@ namespace maketeam.Models
         private static void Desconectar(SqlConnection con)
         {
             con.Close();
+        }
+
+        public static Boolean Loguearse(Usuario User)
+        {
+            bool Logueado;
+            SqlConnection Con = Conectar();
+            SqlCommand consulta = Con.CreateCommand();
+            consulta.CommandType = System.Data.CommandType.Text;
+            consulta.CommandText = "Select * from Usuarios where NombreDeUsuario LIKE '" + User.NombreDeUsuario + "' and Contraseña LIKE '" + User.Contraseña + "'";
+            SqlDataReader lector = consulta.ExecuteReader();
+            Usuario Us = new Usuario();
+            if (lector.Read())
+            {
+                Logueado = true;
+            }
+            else
+            {
+                Logueado = false;
+            }
+            return Logueado;
         }
     }
 }
